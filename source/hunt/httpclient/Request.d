014e0d1e5cde9dcdfa8aa1b87c3a6b4d935a7716
heromyth <bitworld@qq.com>
2020-08-11 16:10:22 +0800

    More api for put, delete

--- source/hunt/httpclient/Request.d
+++ source/hunt/httpclient/Request.d
@@ -42,7 +42,7 @@ class Request {
     /**
      * The number of times to try the request.
      */
-    private int _tries = 1;    
+    private int _tries = 1;
 
     /**
      * The number of milliseconds to wait between retries.
@@ -56,25 +56,25 @@ class Request {
      * Indicate the request contains JSON.
      */
     Request asJson() {
-        _bodyFormat = BodyFormat.JSON; 
+        _bodyFormat = BodyFormat.JSON;
         contentType(MimeType.APPLICATION_JSON_VALUE);
         return this;
     }
-    
+
     /**
      * Indicate the request contains form parameters.
      */
     Request asForm() {
-        _bodyFormat = BodyFormat.FormParams; 
+        _bodyFormat = BodyFormat.FormParams;
         contentType(MimeType.APPLICATION_X_WWW_FORM_VALUE);
         return this;
     }
-    
+
     /**
      * Specify the body format of the request.
      */
     Request bodyFormat(string value) {
-        _bodyFormat = value; 
+        _bodyFormat = value;
         return this;
     }
 
@@ -82,7 +82,7 @@ class Request {
      * Indicate the request is a multi-part form request.
      */
     Request asMultipart() {
-        _bodyFormat = BodyFormat.Multipart; 
+        _bodyFormat = BodyFormat.Multipart;
         return this;
     }
 
@@ -122,7 +122,7 @@ class Request {
     }
 
     Request withHeaders(string[string] headers) {
-        foreach(string key, string value; headers) {
+        foreach (string key, string value; headers) {
             _headers[key] = value;
         }
         return this;
@@ -156,12 +156,12 @@ class Request {
     Response get(string url) {
         Response res;
         .retry(_tries, (int attempts) { res = doGet(url); }, _retryDelay);
-        return res;        
+        return res;
     }
 
     private Response doGet(string url) {
         HttpClientOptions options = new HttpClientOptions();
-        if(_timeout > Duration.zero) {
+        if (_timeout > Duration.zero) {
             options.getTcpConfiguration().setIdleTimeout(_timeout);
             options.getTcpConfiguration().setConnectTimeout(_timeout);
         }
@@ -173,8 +173,8 @@ class Request {
 
         RequestBuilder builder = new RequestBuilder();
 
-        if(_headers !is null) {
-            foreach(string name, string value; _headers) {
+        if (_headers !is null) {
+            foreach (string name, string value; _headers) {
                 builder.addHeader(name, value);
             }
         }
@@ -185,24 +185,24 @@ class Request {
         return new Response(response);
     }
 
-    Response post(string url) {
-        string contentType = _contentType;
-        HttpBody httpBody;
+    /* #region private methods  */
 
-        if(_bodyFormat == BodyFormat.Multipart) {
+    private HttpBody buildBody() {
+        HttpBody httpBody;
+        if (_bodyFormat == BodyFormat.Multipart) {
             MultipartBody.Builder builder = new MultipartBody.Builder();
 
-            foreach(string key, string value; _formData) {
+            foreach (string key, string value; _formData) {
                 builder.addFormDataPart(key, value, MimeType.TEXT_PLAIN_VALUE);
             }
 
-            foreach(ref PendingFile file; _pendingFiles) {
+            foreach (ref PendingFile file; _pendingFiles) {
                 string mimeType = _mimeUtil.getMimeByExtension(file.filename);
-                if(mimeType.empty)
+                if (mimeType.empty)
                     mimeType = MimeType.TEXT_PLAIN_VALUE;
 
-                builder.addFormDataPart(file.name, file.filename, 
-                    HttpBody.createFromFile(mimeType, file.filename));
+                builder.addFormDataPart(file.name, file.filename,
+                        HttpBody.createFromFile(mimeType, file.filename));
 
                 // TODO: Tasks pending completion -@zhangxueping at 2020-05-15T16:41:19+08:00
                 // Add headers
@@ -210,61 +210,15 @@ class Request {
 
             httpBody = builder.build();
         } else {
-            httpBody = HttpBody.create(_contentType, cast(const(ubyte)[])null);
+            httpBody = HttpBody.create(_contentType, cast(const(ubyte)[]) null);
         }
-        
-        return post(url, httpBody);
+
+        return httpBody;
     }
 
-    Response post(string url, string[string] data) {
-        assert(data !is null, "No data avaliable!");
-
-        UrlEncoded encoder = new UrlEncoded;
-        foreach(string name, string value; data) {
-            encoder.put(name, value);
-        }
-        string content = encoder.encode();
-        if(_contentType.empty)
-            _contentType = MimeType.APPLICATION_X_WWW_FORM_VALUE;
-        return post(url, _contentType, cast(const(ubyte)[])content);
-    }
-
-    Response post(string url, JSONValue json) {
-        if(_contentType.empty)
-            _contentType = MimeType.APPLICATION_JSON_VALUE;
-        return post(url, _contentType, cast(const(ubyte)[])json.toString());
-    } 
-
-    Response post(string url, string contentType, const(ubyte)[] content) {
-        Response res;
-        .retry(_tries, (int attempts) {
-                res = doPost(url, contentType, content);
-            }, 
-            _retryDelay);
-        return res;
-    }
-
-    private Response doPost(string url, string contentType, const(ubyte)[] content) {
-        HttpBody hb = HttpBody.create(contentType, content);
-        RequestBuilder builder = new RequestBuilder();
-
-        return doPost(url, hb);
-    }
-
-    Response post(string url, HttpBody httpBody) {
-        Response res;
-
-        .retry(_tries, (int attempts) {
-                res = doPost(url, httpBody);
-            }, 
-            _retryDelay);
-
-        return res;
-    }
-
-    private Response doPost(string url, HttpBody httpBody) {
+    private Response perform(string url, HttpBody httpBody, HttpMethod method = HttpMethod.POST) {
         HttpClientOptions options = new HttpClientOptions();
-        if(_timeout > Duration.zero) {
+        if (_timeout > Duration.zero) {
             options.getTcpConfiguration().setIdleTimeout(_timeout);
             options.getTcpConfiguration().setConnectTimeout(_timeout);
         }
@@ -276,41 +230,248 @@ class Request {
 
         RequestBuilder builder = new RequestBuilder();
 
-        if(_headers !is null) {
-            foreach(string name, string value; _headers) {
+        if (_headers !is null) {
+            foreach (string name, string value; _headers) {
                 builder.addHeader(name, value);
             }
         }
 
-        HttpClientRequest request = builder.url(url).post(httpBody).build();
+        builder.url(url);
+
+        if(method == HttpMethod.POST) 
+            builder.post(httpBody);
+        else if(method == HttpMethod.PUT) 
+            builder.put(httpBody);
+        else if(method == HttpMethod.DELETE) 
+            builder.del(httpBody);
+        else if(method == HttpMethod.PATCH) 
+            builder.patch(httpBody);
+        else
+            throw new Exception("Unsupported method: " ~ method.toString());
+
+        HttpClientRequest request = builder.build();
         HttpClientResponse response = client.newCall(request).execute();
 
         return new Response(response);
     }
+
+    /* #endregion */
+    
+    /* #region POST */
+
+    Response post(string url) {
+        return post(url, buildBody());
+    }
+
+    Response post(string url, string[string] data) {
+        assert(data !is null, "No data avaliable!");
+
+        UrlEncoded encoder = new UrlEncoded;
+        foreach (string name, string value; data) {
+            encoder.put(name, value);
+        }
+        string content = encoder.encode();
+        if (_contentType.empty)
+            _contentType = MimeType.APPLICATION_X_WWW_FORM_VALUE;
+        return post(url, _contentType, cast(const(ubyte)[]) content);
+    }
+
+    Response post(string url, JSONValue json) {
+        if (_contentType.empty)
+            _contentType = MimeType.APPLICATION_JSON_VALUE;
+        return post(url, _contentType, cast(const(ubyte)[]) json.toString());
+    }
+
+    Response post(string url, string contentType, const(ubyte)[] content) {
+        Response res;
+        .retry(_tries, 
+            (int attempts) { 
+                res = perform(url, HttpBody.create(contentType, content), HttpMethod.POST); 
+            }, 
+            _retryDelay);
+        return res;
+    }
+
+    Response post(string url, HttpBody httpBody) {
+        Response res;
+        .retry(_tries, 
+            (int attempts) { 
+                res = perform(url, httpBody, HttpMethod.POST); 
+            }, 
+            _retryDelay);
+        return res;
+    }
+
+    /* #endregion */
+
+
+    /* #region PUT */
+
+    Response put(string url) {
+        return put(url, buildBody());
+    }
+
+    Response put(string url, string[string] data) {
+        assert(data !is null, "No data avaliable!");
+
+        UrlEncoded encoder = new UrlEncoded;
+        foreach (string name, string value; data) {
+            encoder.put(name, value);
+        }
+        string content = encoder.encode();
+        if (_contentType.empty)
+            _contentType = MimeType.APPLICATION_X_WWW_FORM_VALUE;
+        return put(url, _contentType, cast(const(ubyte)[]) content);
+    }
+
+    Response put(string url, JSONValue json) {
+        if (_contentType.empty)
+            _contentType = MimeType.APPLICATION_JSON_VALUE;
+        return put(url, _contentType, cast(const(ubyte)[]) json.toString());
+    }
+
+    Response put(string url, string contentType, const(ubyte)[] content) {
+        Response res;
+        .retry(_tries, 
+            (int attempts) { 
+                res = perform(url, HttpBody.create(contentType, content), HttpMethod.PUT); 
+            }, 
+            _retryDelay);
+        return res;
+    }
+
+    Response put(string url, HttpBody httpBody) {
+        Response res;
+        .retry(_tries, 
+            (int attempts) { 
+                res = perform(url, httpBody, HttpMethod.PUT); 
+            }, 
+            _retryDelay);
+        return res;
+    }
+
+    /* #endregion */
+
+
+    /* #region DELETE */
+
+    Response del(string url) {
+        return del(url, buildBody());
+    }
+
+    Response del(string url, string[string] data) {
+        assert(data !is null, "No data avaliable!");
+
+        UrlEncoded encoder = new UrlEncoded;
+        foreach (string name, string value; data) {
+            encoder.put(name, value);
+        }
+        string content = encoder.encode();
+        if (_contentType.empty)
+            _contentType = MimeType.APPLICATION_X_WWW_FORM_VALUE;
+        return del(url, _contentType, cast(const(ubyte)[]) content);
+    }
+
+    Response del(string url, JSONValue json) {
+        if (_contentType.empty)
+            _contentType = MimeType.APPLICATION_JSON_VALUE;
+        return del(url, _contentType, cast(const(ubyte)[]) json.toString());
+    }
+
+    Response del(string url, string contentType, const(ubyte)[] content) {
+        Response res;
+        .retry(_tries, 
+            (int attempts) { 
+                res = perform(url, HttpBody.create(contentType, content), HttpMethod.DELETE); 
+            }, 
+            _retryDelay);
+        return res;
+    }
+
+    Response del(string url, HttpBody httpBody) {
+        Response res;
+        .retry(_tries, 
+            (int attempts) { 
+                res = perform(url, httpBody, HttpMethod.DELETE); 
+            }, 
+            _retryDelay);
+        return res;
+    }
+
+    /* #endregion */
+    
+
+    /* #region PATCH */
+
+    Response patch(string url) {
+        return patch(url, buildBody());
+    }
+
+    Response patch(string url, string[string] data) {
+        assert(data !is null, "No data avaliable!");
+
+        UrlEncoded encoder = new UrlEncoded;
+        foreach (string name, string value; data) {
+            encoder.put(name, value);
+        }
+        string content = encoder.encode();
+        if (_contentType.empty)
+            _contentType = MimeType.APPLICATION_X_WWW_FORM_VALUE;
+        return patch(url, _contentType, cast(const(ubyte)[]) content);
+    }
+
+    Response patch(string url, JSONValue json) {
+        if (_contentType.empty)
+            _contentType = MimeType.APPLICATION_JSON_VALUE;
+        return patch(url, _contentType, cast(const(ubyte)[]) json.toString());
+    }
+
+    Response patch(string url, string contentType, const(ubyte)[] content) {
+        Response res;
+        .retry(_tries, 
+            (int attempts) { 
+                res = perform(url, HttpBody.create(contentType, content), HttpMethod.PATCH); 
+            }, 
+            _retryDelay);
+        return res;
+    }
+
+    Response patch(string url, HttpBody httpBody) {
+        Response res;
+        .retry(_tries, 
+            (int attempts) { 
+                res = perform(url, httpBody, HttpMethod.PATCH); 
+            }, 
+            _retryDelay);
+        return res;
+    }
+
+    /* #endregion */
 }
 
 /**
  * Retry an operation a given number of times.
  */
-private void retry(int times, Action1!int handler, Duration delay = Duration.zero, 
-    Func1!(Exception, bool) precondition = null) {
+private void retry(int times, Action1!int handler, Duration delay = Duration.zero,
+        Func1!(Exception, bool) precondition = null) {
 
     assert(handler !is null, "The handler can't be null");
     int attempts = 0;
 
-    while(true) {
+    while (true) {
         attempts++;
 
         try {
             handler(attempts);
             break;
-        } catch(Exception ex) {
-            version(HUNT_HTTP_DEBUG) tracef("Retrying %d / %d", attempts, times);
-            if(attempts >= times || precondition !is null && !precondition(ex)) {
+        } catch (Exception ex) {
+            version (HUNT_HTTP_DEBUG)
+                tracef("Retrying %d / %d", attempts, times);
+            if (attempts >= times || precondition !is null && !precondition(ex)) {
                 throw ex;
             }
 
-            if(delay > Duration.zero) {
+            if (delay > Duration.zero) {
                 Thread.sleep(delay);
             }
         }
